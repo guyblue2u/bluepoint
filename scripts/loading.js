0e768b5042d4ca9491e9e60137209fd6668f022e
David Montoya <davidmoncas@gmail.com>
2020-07-18 19:35:55 +0200

    only some sounds

--- scripts/loading.js
+++ scripts/loading.js
@@ -85,10 +85,10 @@ var loading = new Phaser.Class({
         this.load.image("copyIcon", "./assets/images/copy.png");
 
         // audios
-        // this.load.audio("start_Sound", "./assets/audio/Start Sound.mp3");
-        // this.load.audio("intro_Synth", "./assets/audio/Bluepoint Intro Synth.mp3");
-        // this.load.audio("intro_Rythm", "./assets/audio/Bluepoint Intro Rhythm.mp3");
-        // this.load.audio("song", "./assets/audio/Gutted.mp3");
+        this.load.audio("start_Sound", "./assets/audio/Start Sound.mp3");
+        this.load.audio("intro_Synth", "./assets/audio/Bluepoint Intro Synth.mp3");
+        this.load.audio("intro_Rythm", "./assets/audio/Bluepoint Intro Rhythm.mp3");
+        this.load.audio("song", "./assets/audio/Gutted.mp3");
         // this.load.audio("loading", "./assets/audio/Bluepoint Loading Sound.m4a");
         // this.load.audio("outro", "./assets/audio/Bluepoint Outro Music.m4a");
         // this.load.audio("map_music", "./assets/audio/Bluepoint Menu Theme.mp3");
