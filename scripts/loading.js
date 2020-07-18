6c4d18b2177a0a4c92c5a26b14daf75a73492907
David Montoya <davidmoncas@gmail.com>
2020-07-19 00:22:41 +0200

    con la cancion Gutted

--- scripts/loading.js
+++ scripts/loading.js
@@ -88,7 +88,7 @@ var loading = new Phaser.Class({
         // this.load.audio("start_Sound", "./assets/audio/Start Sound.m4a");
         // this.load.audio("intro_Synth", "./assets/audio/Bluepoint Intro Synth.mp3");
         // this.load.audio("intro_Rythm", "./assets/audio/Bluepoint Intro Rhythm.mp3");
-        // this.load.audio("song", "./assets/audio/Gutted.mp3");
+         this.load.audio("song", "./assets/audio/Gutted.mp3");
         // this.load.audio("loading", "./assets/audio/Bluepoint Loading Sound.m4a");
         // this.load.audio("outro", "./assets/audio/Bluepoint Outro Music.m4a");
         //  this.load.audio("map_music", "./assets/audio/Bluepoint Menu Theme.mp3");
